310e62321e1df867c932ff6d755fee40d9bb3ce1
Benoit Lamonica <ben33127@gmail.com>
2021-10-27 16:14:51 +0200

    feature(GrainedBG.vue): Added grained bg, and some scss

--- src/filters/useFilters.js
+++ src/filters/useFilters.js
@@ -3,7 +3,134 @@ export default () => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
+    const grained = (ele, opt) => {
+
+        let element = null,
+            elementId = null,
+            selectorElement = null;
+
+        element = ele
+        elementId = element.id;
+
+
+        //set style for parent
+        if (element.style.position !== 'absolute') {
+            element.style.position = 'relative';
+        }
+        element.style.overflow = 'hidden';
+
+        let prefixes = ["", "-moz-", "-o-animation-", "-webkit-", "-ms-"];
+
+        //default option values
+        let options = {
+            animate: true,
+            patternWidth: 100,
+            patternHeight: 100,
+            grainOpacity: 0.1,
+            grainDensity: 1,
+            grainWidth: 1,
+            grainHeight: 1,
+            grainChaos: 0.5,
+            grainSpeed: 20
+
+        };
+
+        Object.keys(opt).forEach(function (key) {
+            options[key] = opt[key];
+        });
+
+
+        let generateNoise = function () {
+            let canvas = document.createElement('canvas');
+            let ctx = canvas.getContext('2d');
+            canvas.width = options.patternWidth;
+            canvas.height = options.patternHeight;
+            for (let w = 0; w < options.patternWidth; w += options.grainDensity) {
+                for (let h = 0; h < options.patternHeight; h += options.grainDensity) {
+                    let rgb = Math.random() * 256 | 0;
+                    ctx.fillStyle = 'rgba(' + [rgb, rgb, rgb, options.grainOpacity].join() + ')';
+                    ctx.fillRect(w, h, options.grainWidth, options.grainHeight);
+                }
+            }
+            return canvas.toDataURL('image/png');
+        };
+
+        function addCSSRule(sheet, selector, rules, index) {
+            let ins = '';
+            if (selector.length) {
+                ins = selector + "{" + rules + "}";
+            } else {
+                ins = rules;
+            }
+
+            if ("insertRule" in sheet) {
+                sheet.insertRule(ins, index);
+            } else if ("addRule" in sheet) {
+                sheet.addRule(selector, rules, index);
+            }
+        }
+
+
+        let noise = generateNoise();
+
+        let animation = '',
+            keyFrames = ['0%:-10%,10%', '10%:-25%,0%', '20%:-30%,10%', '30%:-30%,30%', '40%::-20%,20%', '50%:-15%,10%', '60%:-20%,20%', '70%:-5%,20%', '80%:-25%,5%', '90%:-30%,25%', '100%:-10%,10%'];
+
+        let pre = prefixes.length;
+        while (pre--) {
+            animation += '@' + prefixes[pre] + 'keyframes grained{';
+            for (let key = 0; key < keyFrames.length; key++) {
+                let keyVal = keyFrames[key].split(':');
+                animation += keyVal[0] + '{';
+                animation += prefixes[pre] + 'transform:translate(' + keyVal[1] + ');';
+                animation += '}';
+            }
+            animation += '}';
+        }
+
+        //add animation keyframe
+        let animationAdded = document.getElementById('grained-animation');
+        if (animationAdded) {
+            animationAdded.parentElement.removeChild(animationAdded);
+        }
+        let style = document.createElement("style");
+        style.type = "text/css";
+        style.id = 'grained-animation';
+        style.innerHTML = animation;
+        document.body.appendChild(style);
+
+        //add custimozed style
+        let styleAdded = document.getElementById('grained-animation-' + elementId);
+        if (styleAdded) {
+            styleAdded.parentElement.removeChild(styleAdded);
+        }
+
+        style = document.createElement("style");
+        style.type = "text/css";
+        style.id = 'grained-animation-' + elementId;
+        document.body.appendChild(style);
+
+        let rule = 'background-image: url(' + noise + ');';
+        rule += 'position: absolute;content: "";height: 300%;width: 300%;left: -100%;top: -100%;';
+        pre = prefixes.length;
+        if (options.animate) {
+            while (pre--) {
+                rule += prefixes[pre] + 'animation-name:grained;';
+                rule += prefixes[pre] + 'animation-iteration-count: infinite;';
+                rule += prefixes[pre] + 'animation-duration: ' + options.grainChaos + 's;';
+                rule += prefixes[pre] + 'animation-timing-function: steps(' + options.grainSpeed + ', end);';
+            }
+        }
+
+        //selecter element to add grains
+        selectorElement = '#' + elementId + '::before';
+
+
+        addCSSRule(style.sheet, selectorElement, rule);
+    }
+
     return {
-        capitaliseFirstLetter
+        capitaliseFirstLetter,
+        grained
     }
 }
