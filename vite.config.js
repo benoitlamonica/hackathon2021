a6b30b71d7d47f504469e75b6ff95f70e02d82ce
Benoit Lamonica <ben33127@gmail.com>
2021-10-27 14:15:17 +0200

    setup(vite.config.js): Added global variable

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), eslintPlugin()]
+    plugins: [vue(), eslintPlugin()],
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src")
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "@/assets/scss/_variable.scss";`
+            },
+        },
+    },
 })
